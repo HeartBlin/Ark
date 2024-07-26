5cf0b40d35e17fbb59a1e66395b18253c18cf214
HeartBlin <Manea.Emil@proton.me>
2024-07-26 19:27:28 +0000

    Get rid of duplicate declarations related to NVidia

--- hosts/Skadi/system.nix
+++ hosts/Skadi/system.nix
@@ -19,10 +19,6 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
-    services.xserver.videoDrivers = [ "nvidia" ];
-
-    services.xserver.enable = true;
-
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
 
