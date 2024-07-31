6929abc26d20635733135c020a36aff7cd5b7209
HeartBlin <Manea.Emil@proton.me>
2024-07-31 13:03:25 +0300

    Switched to using systemd-init since it works now?

--- modules/kernel/main.nix
+++ modules/kernel/main.nix
@@ -12,8 +12,5 @@ in {
     "Skadi" = pkgs.linuxPackages_cachyos;
   }."${hostName}";
 
-  # For some reason with this enabled it waits for
-  # a device which does not exist
-  # TODO look into wtf this tries to do
-  boot.initrd.systemd.enable = mkForce false;
+  boot.initrd.systemd.enable = mkForce true;
 }
