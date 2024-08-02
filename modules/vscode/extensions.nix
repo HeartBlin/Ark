dc31740cc67274a6da36328ae8d8496dceed7d17
HeartBlin <Manea.Emil@proton.me>
2024-08-02 16:28:51 +0300

    Chore

--- modules/vscode/extensions.nix
+++ modules/vscode/extensions.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
 
 {
-  programs.vscode.extensions = with pkgs.vscode-extensions;
-    [ jnoortheen.nix-ide pkief.material-icon-theme ]
-    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-      name = "Everblush";
-      publisher = "mangeshrex";
-      version = "0.1.1";
-      sha256 = "sha256-hqRf3BGQMwFEpOMzpELMKmjS1eg4yPqgTiHQEwi7RUw=";
-    }];
+  programs.vscode.extensions = [
+    pkgs.vscode-extensions.jnoortheen.nix-ide
+    pkgs.vscode-extensions.pkief.material-icon-theme
+  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+    name = "Everblush";
+    publisher = "mangeshrex";
+    version = "0.1.1";
+    sha256 = "sha256-hqRf3BGQMwFEpOMzpELMKmjS1eg4yPqgTiHQEwi7RUw=";
+  }];
 }
