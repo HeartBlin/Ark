db4a4bba186d1ca78a6b5ac94a36b434da7993f3
HeartBlin <Manea.Emil@proton.me>
2024-08-03 22:04:05 +0300

    Added experimental install script

--- hosts/Skadi/hardware.nix
+++ hosts/Skadi/hardware.nix
@@ -10,24 +10,30 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/56d8916e-1c3c-4abc-95ee-23bc7b6679d6";
+    device = "/dev/disk/by-uuid/df022fb2-088a-4573-ad57-b743077282b9";
     fsType = "btrfs";
     options = [ "subvol=root" "compress=zstd" "noatime" ];
   };
 
   boot.initrd.luks.devices."enc".device =
-    "/dev/disk/by-uuid/12be92c3-3609-451e-908a-99a2da020c54";
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/56d8916e-1c3c-4abc-95ee-23bc7b6679d6";
-    fsType = "btrfs";
-    options = [ "subvol=home" "compress=zstd" "noatime" ];
-  };
+    "/dev/disk/by-uuid/72e46fb3-d934-4453-be71-343f5f87bd27";
 
   fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/56d8916e-1c3c-4abc-95ee-23bc7b6679d6";
+    device = "/dev/disk/by-uuid/df022fb2-088a-4573-ad57-b743077282b9";
     fsType = "btrfs";
     options = [ "subvol=nix" "compress=zstd" "noatime" ];
+  };
+
+  fileSystems."/swap" = {
+    device = "/dev/disk/by-uuid/df022fb2-088a-4573-ad57-b743077282b9";
+    fsType = "btrfs";
+    options = [ "subvol=swap" "noatime" ];
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/df022fb2-088a-4573-ad57-b743077282b9";
+    fsType = "btrfs";
+    options = [ "subvol=home" "compress=zstd" "noatime" ];
   };
 
   fileSystems."/boot" = {
@@ -37,7 +43,7 @@
   };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/48850e12-b89d-4aae-8d2b-1e76a139019d"; }];
+    [{ device = "/swap/swapfile"; }];
 
   networking.useDHCP = lib.mkDefault true;
 
