178ce1062cfb459192d7b58a27da5ddb64adb4e0
HeartBlin <Manea.Emil@proton.me>
2024-08-09 19:31:42 +0300

    Added wallpapers

--- packages/Walls/default.nix
+++ packages/Walls/default.nix
@@ -13,10 +13,15 @@ pkgs.stdenvNoCC.mkDerivation {
     hash = "sha256-1hma76HtQXrMlB8Wvaav2Ua+7BYPaesFWCXZp8G5PbI=";
   };
 
-  spiral = pkgs.fetchurl {
+  spiral1 = pkgs.fetchurl {
     url =
       "https://drive.usercontent.google.com/download?id=0B08Ro4V-3VhoNHNqZWtyZGpfSEk&export=download&resourcekey=0-jTWpHMnTl2OYgvMdJOnmhA";
     hash = "sha256-+f26u+P3i2FWqmjtGjYEOi4/obpY0K4ERG1chtrL2KY=";
+  };
+
+  spiral2 = pkgs.fetchurl {
+    url = "https://drive.usercontent.google.com/download?id=0B08Ro4V-3VhoYnFlNzBGRlNTWFk&export=download&resourcekey=0-mdDr5EkQw1EiOXh96g5eqw";
+    hash = "sha256-xk2SaXyzjHk9yzsaW4EH47CfWkGdJRqE86pDWkZi7g0=";
   };
 
   phases = [ "installPhase" ];
@@ -27,7 +32,8 @@ pkgs.stdenvNoCC.mkDerivation {
     mkdir -p $out/share/wallpapers
     cp $tn $out/share/wallpapers/tokyo-night.png
     cp $cf $out/share/wallpapers/crimson-foliage.jpg
-    cp $spiral $out/share/wallpapers/Spiral_6A.jpg
+    cp $spiral1 $out/share/wallpapers/Spiral_6A.jpg
+    cp $spiral2 $out/share/wallpapers/Spiral_3A.jpg
 
     runHook postInstall
   '';
